df3965760b43dcc1ca9c75d6fee1991ba832db61
Bryan Ricker <bricker88@gmail.com>
2013-08-15 13:53:32 -0700

    Add the new date helpers I talked so much about in the last commit

--- js/helpers.js
+++ js/helpers.js
@@ -1,3 +1,10 @@
-Ember.Handlebars.registerBoundHelper('date', function(date) {
+Ember.Handlebars.helper('relativeDate', function(date) {
   return moment(date).fromNow();
 });
+
+
+Ember.Handlebars.helper('date', function(date) {
+  return moment(date).format('MMMM Do YYYY, h:mma');
+});
+
+
