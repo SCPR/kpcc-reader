f67b31d7f7c1e5b0a51016d36a4c774dcaf37ec7
Bryan Ricker <bricker88@gmail.com>
2013-09-14 14:52:29 -0700

    Add some documentation for helpers

--- js/helpers.js
+++ js/helpers.js
@@ -1,13 +1,19 @@
+// {{relativeDate Date}}
+// Returns the relative date from now.
 Ember.Handlebars.helper('relativeDate', function(date) {
   return moment(date).fromNow();
 });
 
 
+// {{date Date}}
+// Returns the absolute date.
 Ember.Handlebars.helper('date', function(date) {
   return moment(date).format('MMMM Do YYYY, h:mma');
 });
 
 
+// {{asset String this}}
+// Returns the asset IMG tag for the requested size.
 Ember.Handlebars.registerHelper('asset', function(size) {
     var url;
     url = Ember.Handlebars.helpers.assetUrl.apply(this, [size]);
@@ -16,11 +22,16 @@ Ember.Handlebars.registerHelper('asset', function(size) {
 }, 'assets');
 
 
+// {{assetUrl String this}}
+// Returns the asset URL for the requested size.
 Ember.Handlebars.registerHelper('assetUrl', function(size) {
     if(!this.get('assets.length')) return "";
     return this.get('assets.firstObject.'+size+'.url');
 }, 'assets');
 
+
+// {{audio this}}
+// Renders the article's primary audio as an AUDIO element.
 Ember.Handlebars.registerHelper('audio', function() {
     if(!this.get('audio.length')) return "";
     url = this.get('audio.firstObject.url');
