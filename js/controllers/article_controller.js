6321207a23fd77921723725a3d9013055622c9a2
Bryan Ricker <bricker88@gmail.com>
2013-10-10 16:56:43 -0700

    Half-way get next/prev links working

--- js/controllers/article_controller.js
+++ js/controllers/article_controller.js
@@ -1,4 +1,30 @@
 Kpcc.ArticleController = Ember.ObjectController.extend({
+    needs: ['articles'],
+
+    nextArticle: function() {
+        var article = this.shiftArticles(1);
+        this.transitionToRoute('article', article)
+    },
+
+    previousArticle: function() {
+        var article = this.shiftArticles(-1);
+        this.transitionToRoute('article', article)
+    },
+
+    shiftArticles: function(delta) {
+        var articles, index;
+
+        articles = this.get('articles');
+        index    = articles.indexOf(this.get('content')) + delta;
+
+        if(index >= 0 && index <= articles.get('length') - 1) {
+            return articles.objectAt(index)
+        } else {
+            return null;
+        }
+    },
+
+
     imgWrapperStyle: function() {
         var assets, url;
 
@@ -8,4 +34,4 @@ Kpcc.ArticleController = Ember.ObjectController.extend({
         url = assets.get('firstObject.small.url');
         return "background:url("+url+") no-repeat;background-size:cover;";
     }.property('assets.@each')
-});
+})
--- js/router.js
+++ js/router.js
@@ -1,29 +1,38 @@
 Kpcc.Router.map(function() {
-    this.resource('articles', { path: "/" });
-    this.resource('article', { path: ":article_id" });
-});
+    this.resource('articles', { path: "/" })
+    this.resource('article', { path: ":article_id" })
+})
 
 
 Kpcc.ArticleRoute = Ember.Route.extend({
+    setupController: function(controller, model) {
+        this.controllerFor('articles').loadMore()
+
+        return controller.setProperties({
+            model       : model,
+            articles    : Kpcc.Article.all()
+        })
+    },
+
     model: function(params) {
         return Kpcc.Article.find(params.article_id);
     }
-});
+})
 
 
 Kpcc.ArticlesRoute = Ember.Route.extend({
     model: function() {
         this.controllerFor('articles').loadMore()
-        return Kpcc.Article.all();
+        return Kpcc.Article.all()
     },
 
     activate: function() {
-        Kpcc.AutoRefresh.start(this.controllerFor('articles'), 60);
-        this._super.apply(this, arguments);
+        Kpcc.AutoRefresh.start(this.controllerFor('articles'), 60)
+        this._super.apply(this, arguments)
     },
 
     deactivate: function() {
-        Kpcc.AutoRefresh.stop();
-        this._super.apply(this, arguments);
+        Kpcc.AutoRefresh.stop()
+        this._super.apply(this, arguments)
     }
-});
+})
